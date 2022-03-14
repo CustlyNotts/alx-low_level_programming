503da90facba7303b76de83ed838211de7bec562
CustlyNotts <kenrovers95@gmail.com>
2022-03-14 18:16:40 +0000

    PLREWE

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	putchar(alphabet);
+	putchar(\n);
+	return (0);
+}
