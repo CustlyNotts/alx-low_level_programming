b6f8918eff3e49902fb63ddf0a7787c0bbe30ffa
CustlyNotts <kenrovers95@gmail.com>
2022-03-15 04:32:30 +0000

    POPOPOPO

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,14 +11,14 @@ int main(void)
 
 	for (i = 48; i <= 56; i++)
 	{
-		for (j = 49; j < 57; j++)
+		for (j = 49; j <= 57; j++)
 		{
 			if (i < j)
 			{
 				putchar(i);
 				putchar(j);
 				if (i != 56 || j != 57)
-				{	
+				{
 					putchar(',');
 					putchar(' ');
 				}
