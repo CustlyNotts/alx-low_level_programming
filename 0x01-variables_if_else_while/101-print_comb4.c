72fae4f66d355ef641e0d573871dc1fda2024033
CustlyNotts <kenrovers95@gmail.com>
2022-03-15 04:39:35 +0000

    YUTTDD

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 49; j <= 56; j++)
+		{
+			for (k = 50; k <= 57; k++)
+			{
+				if ((i < j) && (j < k))
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 55 || j != 56 || k != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
