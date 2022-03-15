5277233f2f52f39db97722babff5d51307cd1963
CustlyNotts <kenrovers95@gmail.com>
2022-03-15 03:29:39 +0000

    Oligarch

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+	int j = 97;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+	for (j = 97; j < 103; j++)
+		putchar(j);
+	putchar(10);
+	return (0);
+}
