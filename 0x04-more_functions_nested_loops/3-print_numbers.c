b15880c2f6b793fd7fbce09b6863fc6ea040d844
CustlyNotts <kenrovers95@gmail.com>
2022-03-19 03:26:58 +0000

    PYTR

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers 0 to 9
+ *
+ * Return: Always 0 (Success)
  */
 void print_numbers(void)
 {
@@ -11,4 +13,5 @@ void print_numbers(void)
 		_putchar(i + '0');
 	}
 	_putchar('\n');
+	return (0);
 }
