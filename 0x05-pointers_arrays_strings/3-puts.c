b8e4ebad88647740818a0fc4361bb70b72e0b657
CustlyNotts <kenrovers95@gmail.com>
2022-03-21 12:03:00 -0700

    OOI

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - swaps the values
+ * @str: the first argument
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+	puts('\n');
+}
