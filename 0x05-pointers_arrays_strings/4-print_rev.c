368a4d059946a6b000a85611250995ba6cf7d96e
CustlyNotts <kenrovers95@gmail.com>
2022-03-22 11:15:28 -0700

    Trace

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,6 @@
 /**
  * print_rev - prints in reverse
  * @s: the first argument
- * Return: Always 0 (Success)
  */
 void print_rev(char *s)
 {
@@ -16,7 +15,7 @@ void print_rev(char *s)
 		ln++;
 		i++;
 	}
-	for (j = (ln - 1); j >= 0; j++)
+	for (j = ln - 1; j >= 0; j++)
 	{
 		_putchar(s[j]);
 	}
