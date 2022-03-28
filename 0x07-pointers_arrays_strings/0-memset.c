0436d4ae78c8019c2422f4c7b404a762b85c85fe
CustlyNotts <kenrovers95@gmail.com>
2022-03-28 18:09:16 +0000

    The first plug n play

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a const byte
+ * @s: first whatever
+ * @b: second whatever
+ * @n: third whatever
+ *
+ * Return: a pointer again
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+	char *add, chanceArray[n];
+	
+	add = &chanceArray;
+	while (i < n)
+	{
+		chanceArray[i] = b;
+		i++
+	}
+	*s = chanceArray;
+	return (add);
+}
