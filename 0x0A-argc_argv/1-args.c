dd42ef2b9f3a53b6ea33af21e973defadba4eaf0
CustlyNotts <kenrovers95@gmail.com>
2022-04-03 15:26:07 +0000

    Task n1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - the program starts here
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
