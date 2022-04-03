d1fc4f7a25c40c49fc0e6a8cc21e476c1f403c0d
CustlyNotts <kenrovers95@gmail.com>
2022-04-03 14:36:13 +0000

    Na wa o

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argv: argument vector
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
+	(void)argc;
 	_putchar(argv[0]);
 	_putchar('\n');
 	return (0);
