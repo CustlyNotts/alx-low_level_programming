9b6d999ec2162e4d1e377ffe7e9f171ac615510e
CustlyNotts <kenrovers95@gmail.com>
2022-04-05 18:43:44 +0000

    The concat

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates the trings s1 & s2
+ * @s1: string 1
+ * @s2: string 2
+ * Return: NULL (Failure) or Pointer address
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int size1 = 0, size2 = 0, i = 0, j, sum;
+	char *ptr;
+
+	if (s1 == NULL)
+		size1 = 0;
+	if (s2 == NULL)
+		size2 = 0;
+	for (; s1[size1]; size1++)
+		;
+	for (; s2[size2]; size2++)
+		;
+	sum = size1 + size2;
+	ptr = malloc(sizeof(char) * sum + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (; i < size1; i++)
+		ptr[i] = s1[i];
+	for (; j < size2; j++)
+		ptr[j + size1] = s2[j];
+	ptr[sum] = '\0'
+	return (ptr);
+}
