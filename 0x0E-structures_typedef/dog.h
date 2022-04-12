3cf53e4489bd286ba43a4853b6a0ab1d03ef6e26
CustlyNotts <kenrovers95@gmail.com>
2022-04-12 00:01:36 +0000

    My first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - the new struct
+ * @*name: the name
+ * @age: the age noni
+ * @*owner: whoever owns the dog
+ * Description: Just a dog I think
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
