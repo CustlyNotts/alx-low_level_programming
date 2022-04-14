efb24e0fce95bfe3a3bd9e67cba1f81b9c303bff
CustlyNotts <kenrovers95@gmail.com>
2022-04-14 00:36:11 +0000

    Chump

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - the main guy
+ * @array: teh array
+ * @size: size of array
+ * @cmp: compare function
+ * Return: -1 or 1 or int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (array != NULL && cmp != NULL)
+		for (i = 0; i < size; i++)
+			if (cmp(array[i]) != 0)
+				return (i);
+		return (-1);
+}
