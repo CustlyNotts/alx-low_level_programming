6f336aa0908a2863edc38e84edf82de261529a6a
CustlyNotts <kenrovers95@gmail.com>
2022-04-30 09:22:38 +0000

    The fix for sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,8 +15,9 @@ va_list list;
 unsigned int i, sum = 0;
 
 va_start(list, n);
-if (n != 0)
-	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+if (n == 0)
+	return (0);
+for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
 	;
 va_end(list);
 
